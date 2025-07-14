87f2b24cf98ba098c9cd8369741bf60cebdc2631
thanhnhan-12 <nguyenthanhnhan12701@gmail.com>
2025-07-14 23:01:11 +0700

    validate create-user-dto & connecting dto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,8 +1,19 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
     id: number;
+
+    @IsString({ message: "Name should be a string value." })
+    @IsNotEmpty()
+    @MinLength(3, { message: "Name should have a minimum of 3 characters." })
     name: string;
+
+    @IsEmail()
     email: string;
-    age: number;
-    gender: string;
+
+    @IsString()
+    @IsOptional()
+    gender?: string;
+
     isMarried: boolean;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, ValidationPipe } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dtos/create-user.dto";
 
 // http://localhost:3000/users
 
 @Controller('users')
 export class UsersController {
+    usersService: UsersService;
+
+    constructor() {
+        this.usersService = new UsersService();
+    }
 
     @Get()
     getUsers(@Query() query: string) {
-        const usersService = new UsersService();
-        return usersService.getAllUsers();
+        return this.usersService.getAllUsers();
     }
 
     @Get(":id")
@@ -18,15 +23,12 @@ export class UsersController {
         @Query('page', ParseIntPipe) page: number,
     ) {
 
-        const usersSevice = new UsersService();
-        return usersSevice.getUserById(+id);
+        return this.usersService.getUserById(id);
     }
 
     @Post()
-    createUser() {
-        const user = { id: 3, name: "Klopp", email: "klopp@gmail.com", age: 55, gender: "male", isMarried: true }
-        const usersService = new UsersService();
-        usersService.createUser(user);
+    createUser(@Body() user: CreateUserDto) {
+        // this.usersService.createUser(user);
         return "A new user has been created!";
     }
 }
