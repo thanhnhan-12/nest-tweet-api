9d3ecb66f33947827c0d93d65a1843b75c6e45a5
thanhnhan-12 <nguyenthanhnhan12701@gmail.com>
2025-08-02 22:40:31 +0700

    connection mysql with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { TweetModule } from './tweet/tweet.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [UsersModule, TweetModule, AuthModule, TypeOrmModule.forRoot({
+    type: "mysql",
+    entities: [],
+    synchronize: true,
+    host: "localhost",
+    port: 3306,
+    username: "user",
+    password: "password",
+    database: "tweet_api",
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
